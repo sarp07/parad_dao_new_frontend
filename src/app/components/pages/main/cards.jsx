4ca8829baa436d11a2287419ffabe5c8d361c797
nekwake <200706010@st.maltepe.edu.tr>
2024-12-05 16:13:23 +0300

    .

--- src/app/components/pages/main/cards.jsx
+++ src/app/components/pages/main/cards.jsx
@@ -2,82 +2,85 @@ import React from "react";
 import Container from "../../common/container";
 
 const cards = [
-   {
-      title: "Parad Wallet",
-      description: "Coming soon",
-      link: null,
-      img: "https://s13.gifyu.com/images/SJNTk.gif",
-   },
-   {
-      title: "Parad Dao",
-      description: "Parad Dao",
-      link: "https://app.prddao.io",
-      img: "https://s13.gifyu.com/images/SJNTk.gif",
-   },
-   {
-      title: "Parad Finance",
-      description: "Coming soon",
-      link: null,
-      img: "https://s13.gifyu.com/images/SJNTk.gif",
-   },
-   {
-      title: "Parad Cheque",
-      description: "Coming soon",
-      link: null,
-      img: "https://s13.gifyu.com/images/SJNTk.gif",
-   },
+  {
+    title: "Parad Wallet",
+    description: "Coming soon",
+    link: null,
+    img: "https://s13.gifyu.com/images/SJNTk.gif",
+  },
+  {
+    title: "Parad Dao",
+    description: "Parad Dao",
+    link: "https://app.prddao.io",
+    img: "https://s7.gifyu.com/images/SJTE3.gif",
+  },
+  {
+    title: "Parad Finance",
+    description: "Coming soon",
+    link: null,
+    img: "https://s13.gifyu.com/images/SJTEn.gif",
+  },
+  {
+    title: "Parad Cheque",
+    description: "Coming soon",
+    link: null,
+    img: "https://s13.gifyu.com/images/SJTEc.gif",
+  },
 ];
 
 const Cards = () => {
-   return (
-      <div className="cards w-full flex">
-         <Container>
-            <div className="grid lg:grid-cols-2 grid-cols-1 justify-center gap-x-8 gap-y-20 mt-20">
-               {cards.map((card, index) => (
-                  <div
-                     className=" w-full h-auto relative flex flex-col"
-                     key={index}
-                  >
-                     <div className="img-container w-full flex items-center justify-center h-[100px]">
-                        <img
-                           src={card.img}
-                           alt="Card GIF"
-                           className="w-[160px] h-auto object-cover"
-                        />
-                     </div>
-                     <div className="item-box w-full h-[240px] py-6 relative overflow-hidden flex flex-col justify-center gap-4">
-                        <div className="mini-glow absolute right-0 bottom-20 -z-10"></div>
-                        <p className="text-4xl font-black text-center text-[#a2cdef]">
-                           {card.title.toUpperCase()}
-                        </p>
+  return (
+    <div className="cards w-full flex flex-col items-center">
+      <Container>
+        {/* Başlıklar */}
+        <div className="text-center mt-10 mb-10">
+          <p className="text-xl text-white mb-2">DISCOVER OUR PRODUCTS</p>
+          <h5 className="w-full text-4xl lg:text-8xl font-black text-center text-[#a2cdef]">
+            PARAD ECOSYSTEM
+          </h5>
+        </div>
 
-                        <p className="text-xl text-center text-[#d0d0d0] px-16">
-                           {card.link ? (
-                              <a
-                                 href={card.link}
-                                 className="text-blue-500 underline"
-                              >
-                                 {card.description}
-                              </a>
-                           ) : (
-                              "Your self-custody wallet for ultimate security and control over your digital assets."
-                           )}
-                        </p>
-                        <div className="w-full flex items-center justify-center mt-5">
-                           <button
-                              className="text-xl text-center text-[#788fa1] border rounded-[30px] w-auto border-white/10 bg-white/[0.03] px-20 py-[10px]"
-                              disabled={card.link === null}
-                           >
-                              {card.description}
-                           </button>
-                        </div>
-                     </div>
-                  </div>
-               ))}
+        {/* Kartlar */}
+        <div className="grid lg:grid-cols-2 grid-cols-1 justify-center gap-x-8 gap-y-20 mt-10">
+          {cards.map((card, index) => (
+            <div className="w-full h-auto relative flex flex-col" key={index}>
+              <div className="img-container w-full flex items-center justify-center h-[100px]">
+                <img
+                  src={card.img}
+                  alt="Card GIF"
+                  className="w-[160px] h-auto object-cover"
+                />
+              </div>
+              <div className="item-box w-full h-[240px] py-6 relative overflow-hidden flex flex-col justify-center gap-4">
+                <div className="mini-glow absolute right-0 bottom-20 -z-10"></div>
+                <p className="text-4xl font-black text-center text-[#a2cdef]">
+                  {card.title.toUpperCase()}
+                </p>
+
+                <p className="text-xl text-center text-[#d0d0d0] px-16">
+                  {card.link ? (
+                    <a href={card.link} className="text-blue-500 underline">
+                      {card.description}
+                    </a>
+                  ) : (
+                    "Your self-custody wallet for ultimate security and control over your digital assets."
+                  )}
+                </p>
+                <div className="w-full flex items-center justify-center mt-5">
+                  <button
+                    className="text-xl text-center text-[#788fa1] border rounded-[30px] w-auto border-white/10 bg-white/[0.03] px-20 py-[10px]"
+                    disabled={card.link === null}
+                  >
+                    {card.description}
+                  </button>
+                </div>
+              </div>
             </div>
-         </Container>
-      </div>
-   );
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Cards;
--- src/app/components/pages/main/hero.jsx
+++ src/app/components/pages/main/hero.jsx
@@ -7,59 +7,59 @@ import Image from "next/image";
 import HeroBoxImg from "../../../assets/glassmorphism-boxes/herobox.png";
 import ItemBoxImg from "../../../assets/cards/card.png";
 const Hero = () => {
-   return (
-      <div className="main-hero w-full mt-16 h-auto text-white relative">
-         <Container>
-            {/* Parıltı Efekti */}
-            <div className="absolute w-full flex justify-center items-center z-0">
-               <div className="glow w-[500px] h-[500px]"></div>
+  return (
+    <div className="main-hero w-full mt-16 h-auto text-white relative">
+      <Container>
+        {/* Parıltı Efekti */}
+        <div className="absolute w-full flex justify-center items-center z-0">
+          <div className="glow w-[500px] h-[500px]"></div>
+        </div>
+
+        {/* İçerik */}
+        <div className="relative z-10">
+          <div className="flex justify-center mb-10">
+            <img
+              src="https://s7.gifyu.com/images/SJKEh.gif"
+              alt="Hero GIF"
+              className="w-auto h-96 mt-10"
+            />
+          </div>
+
+          <div className="flex justify-center">
+            <h5 className="w-5/6 text-8xl font-black text-center text-[#a2cdef] relative z-[10] lg:block hidden">
+              SECURE THE FUTURE<br></br>OF FINANCE WITH<br></br>PARAD.
+            </h5>
+            <h5 className="w-full text-4xl font-black text-center text-[#a2cdef] relative z-[10] lg:hidden flex">
+              SECURE THE FUTURE OF FINANCE WITH PARAD.
+            </h5>
+            <div className="absolute w-full flex items-center justify-center mt-10">
+              {/* <HeroBox /> */}
+              <Image
+                src={HeroBoxImg}
+                alt="hero-box"
+                className="opacity-30 max-h-[220px] h-full w-auto"
+              />
             </div>
+          </div>
 
-            {/* İçerik */}
-            <div className="relative z-10">
-               <div className="flex justify-center mb-10">
-                  <img
-                     src="https://s7.gifyu.com/images/SJKEh.gif"
-                     alt="Hero GIF"
-                     className="w-auto h-96 mt-10"
-                  />
-               </div>
-
-               <div className="flex justify-center">
-                  <h5 className="w-5/6 text-8xl font-black text-center text-[#a2cdef] relative z-[10] lg:flex hidden">
-                     SECURE THE FUTURE<br></br>OF FINANCE WITH<br></br>PARAD.
-                  </h5>
-                  <h5 className="w-full text-4xl font-black text-center text-[#a2cdef] relative z-[10] lg:hidden flex">
-                     SECURE THE FUTURE OF FINANCE WITH PARAD.
-                  </h5>
-                  <div className="absolute w-full flex items-center justify-center mt-10">
-                     {/* <HeroBox /> */}
-                     <Image
-                        src={HeroBoxImg}
-                        alt="hero-box"
-                        className="opacity-30 max-h-[220px] h-full w-auto"
-                     />
-                  </div>
-               </div>
-
-               <div className="flex justify-center mt-6 space-x-4">
-                  <a
-                     href="#"
-                     className="text-white text-sm bg-[#1f2a3f] px-6 py-2 rounded-full border border-white/[0.15] hover:bg-[#1f2a3f] hover:border-white/[0.5] transition-all"
-                  >
-                     Discover PARAD
-                  </a>
-                  <a
-                     href="https://app.prddao.io"
-                     className="text-white text-sm bg-[#1f2a3f] px-6 py-2 rounded-full border border-white/[0.15] hover:bg-[#1f2a3f] hover:border-white/[0.5] transition-all"
-                  >
-                     Buy PARAD
-                  </a>
-               </div>
-            </div>
-         </Container>
-      </div>
-   );
+          <div className="flex justify-center mt-6 space-x-4">
+            <a
+              href="#"
+              className="text-white text-sm bg-[#1f2a3f] px-6 py-2 rounded-full border border-white/[0.15] hover:bg-[#1f2a3f] hover:border-white/[0.5] transition-all"
+            >
+              Discover PARAD
+            </a>
+            <a
+              href="https://app.prddao.io"
+              className="text-white text-sm bg-[#1f2a3f] px-6 py-2 rounded-full border border-white/[0.15] hover:bg-[#1f2a3f] hover:border-white/[0.5] transition-all"
+            >
+              Buy PARAD
+            </a>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Hero;
