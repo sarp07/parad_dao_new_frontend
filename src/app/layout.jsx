0cfe6d402ff71cc5108257ee80a6c4858869b2eb
nekwake <200706010@st.maltepe.edu.tr>
2024-12-06 00:39:21 +0300

    .

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,18 +3,18 @@ import Header from "./components/common/header";
 import "./globals.css";
 
 export const metadata = {
-   title: "Parad Website",
-   description: "paradpadar",
+  title: "Parad - Your Gateway to Crypto",
+  description: "Track, swap, and manage Parad tokens effortlessly.",
 };
 
 export default function RootLayout({ children }) {
-   return (
-      <html lang="en">
-         <body className={`antialiased overflow-x-hidden`}>
-            <div className="overflow-x-hidden">
-               <Header /> {children} <Footer />
-            </div>
-         </body>
-      </html>
-   );
+  return (
+    <html lang="en">
+      <body className={`antialiased overflow-x-hidden`}>
+        <div className="overflow-x-hidden">
+          <Header /> {children} <Footer />
+        </div>
+      </body>
+    </html>
+  );
 }
